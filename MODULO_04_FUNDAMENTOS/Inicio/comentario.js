a88d04e7eb76e5c679dc64ea34dad9caa5288f09
Valdiran <valdiran47@gmail.com>
2022-11-08 19:19:34 -0300

    foram acionados mais intens

--- MODULO_04_FUNDAMENTOS/Inicio/comentario.js
+++
@@ -1,5 +0,0 @@
-//comando para comentar e descomentar um código em javaScript
-// (Selecione o bloco de código e as tlecas ctrl)
-
-console.log(1 + 3);
-console.log("Meu nome é Valdiran");
---
+++ MODULO_04_FUNDAMENTOS/Inicio/comentarios.js
@@ -0,0 +1,8 @@
+//Comentarios de um unica linha
+console.log("Linha 1");
+/**
+comentarios de 
+multiplas
+linhas
+ */
+console.log("Comentario de multiplas linhas");
